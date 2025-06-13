6c4f41b54635a863dad825f6a86a6dbad9372a78
Vatukah <anupamthakur058@gmail.com>
2025-06-14 02:26:37 +0530

    denpendencies fix

--- client/src/adminpages/users.jsx
+++ client/src/adminpages/users.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import DynamicTable from "../components/dynamicTable";
-import TableSkeleton from "../components/tableSkeleton";
 import { useAdmin } from "../contexts/providers/adminProvider";
 
 export default function Users() {
